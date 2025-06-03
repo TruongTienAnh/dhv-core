f7971d1fe46acb8936b16049bfce6e1ef2366d25
truongtienanh19 <97039585+TruongTienAnh@users.noreply.github.com>
2025-06-03 16:31:15 +0700

    load news

--- controllers/core/front-end/news.php
+++ controllers/core/front-end/news.php
@@ -4,8 +4,51 @@ $jatbi = new Jatbi($app);
 $setting = $app->getValueData('setting');
 
 $app->router("/news", 'GET', function($vars) use ($app, $jatbi, $setting) {
-    
-    
-    echo $app->render('templates/dhv/news.html');
-});
+    // Tiêu đề trang
+    $vars['title'] = $jatbi->lang('Tin tức');
 
+    // Truy vấn 2 danh mục có tổng views cao nhất
+    $top_categories = $app->select("categories", [
+        "[>]news" => ["id" => "category_id"]
+    ], [
+        "categories.id",
+        "categories.name",
+        "categories.slug",
+        "total_views" => $app->raw("SUM(news.views)")
+    ], [
+        "news.status" => 'A',
+        "GROUP" => "categories.id",
+        "ORDER" => ["total_views" => "DESC"],
+        "LIMIT" => 2
+    ]);
+
+    // Với mỗi danh mục, lấy 2 bài viết có views cao nhất
+    $category_posts = [];
+    foreach ($top_categories as $category) {    
+        $posts = $app->select("news", [
+            "id",
+            "title",
+            "slug",
+            "content",
+            "image_url",
+            "views",
+            "published_at"
+        ], [
+            "category_id" => $category['id'],
+            "status" => 'A',
+            "ORDER" => ["views" => "DESC"],
+            "LIMIT" => 2
+        ]);
+        
+        $category_posts[] = [
+            'category' => $category,
+            'posts' => $posts
+        ];
+    }
+
+    // Truyền dữ liệu vào template
+    $vars['category_posts'] = $category_posts;
+
+    echo $app->render('templates/dhv/news.html', $vars);
+});
+?>
